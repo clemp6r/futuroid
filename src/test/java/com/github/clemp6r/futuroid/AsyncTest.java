4dff1a746c88c0b7cd038d3640b142da80abd0f8
Clément Plantier <clemplantier@gmail.com>
2014-03-22 15:57:45 +0100

    Added unit tests

--- src/test/java/com/github/clemp6r/futuroid/AsyncTest.java
+++ src/test/java/com/github/clemp6r/futuroid/AsyncTest.java
@@ -12,7 +12,9 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -50,17 +52,17 @@ public class AsyncTest {
 
     @Test
     public void shouldExecuteUiCallbackOnUiThread() throws ExecutionException, InterruptedException {
-        Future<Thread> future = createNetworkTask();
+        Future<Void> future = Async.immediate(null);
 
         final Holder result = new Holder();
-
-        // wait until it is finished
-        future.get();
-
-        future.addUiCallback(new FutureCallback<Thread>() {
+        future.addUiCallback(new FutureCallback<Void>() {
             @Override
-            public void onSuccess(Thread object) {
+            public void onSuccess(Void object) {
                 result.o = Thread.currentThread();
+
+                synchronized (AsyncTest.this) {
+                    notify();
+                }
             }
 
             @Override
@@ -81,7 +83,6 @@ public class AsyncTest {
         Future<Thread> future = createNetworkTask();
 
         final Holder<Boolean> result = new Holder<Boolean>();
-        result.o = false;
 
         synchronized (result) {
             future.addCallback(new FutureCallback<Thread>() {
@@ -95,6 +96,7 @@ public class AsyncTest {
                 }
                 @Override
                 public void onFailure(Throwable t) {
+                    result.o = false;
                     t.printStackTrace();
 
                     synchronized (result) {
@@ -103,7 +105,9 @@ public class AsyncTest {
                 }
             });
 
-            result.wait(2000);
+            if (result == null)  {
+                result.wait(10000);
+            }
         }
 
         // check that the callback has been executed
@@ -114,6 +118,7 @@ public class AsyncTest {
         return Async.submit(new Callable<T>() {
             @Override
             public T call() throws Exception {
+                Thread.sleep(200);
                 return result;
             }
         });
@@ -173,5 +178,90 @@ public class AsyncTest {
     public void shouldReturnProvidedObject() throws ExecutionException, InterruptedException {
         Future<String> future = createFuture("A").willReturn("B");
         assertEquals("B", future.get());
+    }
+
+    @Test
+    public void shouldCallSuccessCallbackGivenActionSucceed() throws ExecutionException, InterruptedException {
+        Future<String> future = Async.immediate("A");
+
+        final Holder<String> resultHolder = new Holder<String>();
+        final Holder<Throwable> failureHolder = new Holder<Throwable>();
+
+        future.onSuccess(new SuccessCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                resultHolder.o = result;
+            }
+        });
+
+        future.onFailure(new FailureCallback() {
+            @Override
+            public void onFailure(Throwable t) {
+                failureHolder.o = t;
+            }
+        });
+
+        assertEquals("A", resultHolder.o);
+        assertNull(failureHolder.o);
+    }
+
+    @Test
+    public void shouldCallFailureCallbackGivenActionFailure() throws ExecutionException, InterruptedException {
+        Future<String> future = Async.immediateFail(new Exception(("test")));
+
+        final Holder<String> resultHolder = new Holder<String>();
+        final Holder<Throwable> failureHolder = new Holder<Throwable>();
+
+        future.onSuccess(new SuccessCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                resultHolder.o = result;
+            }
+        });
+
+        future.onFailure(new FailureCallback() {
+            @Override
+            public void onFailure(Throwable t) {
+                failureHolder.o = t;
+            }
+        });
+
+        assertNull(resultHolder.o);
+        assertNotNull(failureHolder.o);
+        assertEquals("test", failureHolder.o.getMessage());
+    }
+
+    @Test
+    public void shouldRunUiSuccessCallbackOnMainThread() {
+        Future<String> future = Async.immediate("A");
+
+        final Holder<Thread> threadHolder = new Holder<Thread>();
+
+        future.onUiSuccess(new SuccessCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                threadHolder.o = Thread.currentThread();
+            }
+        });
+
+        assertNotNull("The callback has not been run", threadHolder.o);
+        assertEquals(Thread.currentThread(), threadHolder.o);
+    }
+
+    @Test
+    public void shouldRunUiFailureCallbackOnMainThread() {
+        Future<String> future = Async.immediateFail(new Exception("test"));
+
+        final Holder<Thread> threadHolder = new Holder<Thread>();
+
+        future.onUiFailure(new FailureCallback() {
+            @Override
+            public void onFailure(Throwable t) {
+                threadHolder.o = Thread.currentThread();
+            }
+        });
+
+        assertNotNull("The callback has not been run", threadHolder.o);
+        assertEquals(Thread.currentThread(), threadHolder.o);
     }
 }
